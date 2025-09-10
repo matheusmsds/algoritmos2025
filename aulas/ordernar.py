b52330462575d514c07868b1f7fc8051ef383c01
matheusmsds <20251pvai10030012@estudantes.ifpr.edu.br>
2025-09-09 22:27:30 -0300

    Update ordernar.py

--- aulas/ordernar.py
+++ aulas/ordernar.py
@@ -10,3 +10,12 @@ for i in range(0, len(array) - 1):
             array[j+1] = aux
 
 print(array)
+
+array = [32,9,12,3,68,27,15]
+for i in range(0, len(array) - 1):
+    for j in range(0,len(array) - 1):
+        if(array[j] > array[j+1]):
+            aux = array[j]
+            array[j] = array[j+1]
+            array[j+1] = aux
+    print(f"Fim do {j + 1} ciclo: {array}")
