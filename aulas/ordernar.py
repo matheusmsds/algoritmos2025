63daba62824d26dc4cf66046cfc7ba3c41f6110c
matheusmsds <20251pvai10030012@estudantes.ifpr.edu.br>
2025-09-09 22:31:06 -0300

    Update ordernar.py

--- aulas/ordernar.py
+++ aulas/ordernar.py
@@ -19,3 +19,15 @@ for i in range(0, len(array) - 1):
             array[j] = array[j+1]
             array[j+1] = aux
     print(f"Fim do {j + 1} ciclo: {array}")
+
+array = [32,9,12,3,68,27,15]
+def bubble(vetor):
+    for i in range(0, len(vetor) - 1):
+        for j in range(0,len(vetor) - 1):
+            if(vetor[j] > vetor[j+1]):
+                aux = vetor[j]
+                vetor[j] = vetor[j+1]
+                vetor[j+1] = aux
+    return vetor
+
+print(bubble(array))
